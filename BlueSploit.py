65474ad4c2ef5c7c532fd106c23896ba28c8d80b
Kostas <kostastsale@gmail.com>
2019-08-12 23:29:04 -0700

    Update BlueSploit.py

--- BlueSploit.py
+++ BlueSploit.py
@@ -1,13 +1,36 @@
 import cmd2
 from Gather_info import *
+from data import *
 from colorama import Fore, Back, Style, init
 import cmd2_submenu
+
+class Query_term(cmd2.Cmd):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.prompt = 'Query #> '
+
+    
+    Query_WinEvents = "Query Windows events"
+
+
+    @cmd2.with_category(Query_WinEvents)
+    def do_check_deep_security(self,args):
+        Gather.deepBlue_security()
+        
+    @cmd2.with_category(Query_WinEvents)
+    def do_check_deep_system(self,args):
+        Gather.deepBlue_system()        
+
+    @cmd2.with_category(Query_WinEvents)
+    def do_check_deep_powershell_snippet(self,args):
+        Gather.deepBlue_powershell_snippet() 
+
 
 
 class Gather_term(cmd2.Cmd):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.prompt = 'Gather $> '
+        self.prompt = 'Gather #> '
 
     gather_information = "Gather Information"
 
@@ -21,19 +44,12 @@ class Gather_term(cmd2.Cmd):
         """Gather local user information"""
         Gather.local_usersinfo()
     
-    @cmd2.with_category(gather_information)
-    def do_check_deep_security(self,args):
-        Gather.deepBlue_security()
-        
-    def do_check_deep_system(self,args):
-        Gather.deepBlue_system()        
 
-    def do_check_deep_powershell_snippet(self,args):
-        Gather.deepBlue_powershell_snippet()    
 
+@cmd2_submenu.AddSubmenu(Query_term(),
+                         command='query')
 @cmd2_submenu.AddSubmenu(Gather_term(),
-                         command='gather',
-                 aliases=('g',))
+                         command='gather')
 class BlueSploit(cmd2.Cmd):
     intro = cmd2.style(""" \n ______  _               _____         _         _  _   
 | ___ \| |             /  ___|       | |       (_)| |  
@@ -49,8 +65,8 @@ class BlueSploit(cmd2.Cmd):
         super().__init__(*args, **kwargs)
         self.prompt = (Style.RESET_ALL + Style.BRIGHT  + Back.BLUE + "\nBlueSploit $> "+ Style.RESET_ALL +  Fore.GREEN)    
 
-    def do_deepBlue(self, args):
-        self.poutput(cmd2.style('Deep Blue test', fg='green'))
+    def do_list_modules(self,args):
+      modules()
 
 
 
