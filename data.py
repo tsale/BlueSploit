0eca2985dac8b2d2fac76a3ffcc4d7418eaca3d9
Kostas <kostastsale@gmail.com>
2019-08-14 21:57:33 -0700

    Adding notes capabilities

--- data.py
+++ data.py
@@ -1,6 +1,25 @@
+import csv
+
 
 def modules():
-  mods = {'gather':'Gather Information','query':'Query Windows events'}
-  for k,v in mods.items():
-    print("{}\t=>\t\t{}".format(k,v))
-    
+    mods = {'gather':'Gather Information','query':'Query Windows events'}
+    for k,v in mods.items():
+        print("{}\t=>\t\t{}".format(k,v))
+
+def write_csv():
+    nlist = []
+    ninput = input("Add your note:\n")
+    #ninput = ("{}\n".format(ninput))
+    nlist.append(ninput)
+    with open('notes.csv', 'a',newline='') as csvFile:
+        for x in nlist:
+            writer = csv.writer(csvFile)
+            writer.writerow([x])
+            csvFile.close()
+
+
+def show_notes():
+    with open('notes.csv', 'r') as csvFile:
+        reader = csv.reader(csvFile)
+        for row in reader:
+            print(row)
