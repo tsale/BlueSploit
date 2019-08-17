ae5f94c82245e87020fccb3b2bc138ed705bcb97
Kostas <kostastsale@gmail.com>
2019-08-17 11:47:45 -0700

    Added - Network module functionality
    Detailed network listening processes representation with their hash value

--- Gather_info.py
+++ Gather_info.py
@@ -1,71 +1,49 @@
-import subprocess
-from colorama import Fore, Back, Style
-from data import *
+import csv
+import time
+import socket
+import os
 
-green = Fore.GREEN
-reset = Fore.RESET
 
-class Gather():
-    def systeminfo():
-        ## run and print systeminfo results
-        print(green+"\n\tLocal System Information: \n"+reset)
-        sysinfo = subprocess.run("sysinternals\psinfo -accepteula -s -h -d",shell=True,stdout=subprocess.PIPE).stdout.decode('utf-8')
-        print(sysinfo)
-        
-        ## Write results to file
-        args = str(sysinfo)
-        Files.mk_file("system-info.txt",sysinfo)
-        return(sysinfo)
-    
-    def local_usersinfo():
-        ## run and print UserInfo results
-        print(green+"\n\tUsers Information: \n"+reset)        
-        userInfo = subprocess.run("wmic useraccount get name,SID,Status\n",shell=True,stdout=subprocess.PIPE).stdout.decode('utf-8')
-        print(userInfo)
-        
-        ## run and print localAdmins results
-        print(green + "\n\tLocal Users and Administrators: " + reset)     
-        localAdmins = subprocess.run("powershell.exe Get-LocalGroupMember -Group Administrators\n",shell=True,stdout=subprocess.PIPE).stdout.decode('utf-8')
-        print(localAdmins)
-        
-        ## Write results to file
-        args = ("{}{}".format(userInfo,localAdmins))
-        Files.mk_file("user-info.txt",args)
-        
-        return(userInfo,localAdmins)
-    
-    
-class DeepBlue():    
-    def deepBlue_security():
-        security  = subprocess.call("""powershell.exe "DeepBlueCLI\DeepBlue.ps1 -log security| Out-Host -Paging""",shell=True)
-        return(security)
-    def deepBlue_system():
-        system = subprocess.call("""powershell.exe "DeepBlueCLI\DeepBlue.ps1 -log security | Out-Host -Paging""",shell=True)
-        return(system)
-    def deepBlue_powershell():
-        powershell = subprocess.call("""powershell.exe "DeepBlueCLI\DeepBlue.ps1 -log powershell | Out-Host -Paging""",shell=True)
-        return(powershell)
-    
-    
-class Network_checks():
-    def netstat_info():
-        info = subprocess.run("""powershell.exe "netstat -ant | select -skip 4 | ConvertFrom-String -PropertyNames none, proto,ipsrc,ipdst,state,state2,none,none | select ipsrc,ipdst,state" """,shell=True,stdout=subprocess.PIPE).stdout.decode('utf-8')
-        print(info)
-        Files.mk_file("netstat-info.txt",info)
-        
-        return(info)
-    
-    def netstat_listening():    
-        listening_processes = subprocess.run("""powershell.exe "netstat -ano | findstr -i listening | ForEach-Object { $_ -split '\s+|\t+' } | findstr /r '^[1-9+]*$' | sort | unique | ForEach-Object { Get-Process -Id $_ } | Select ProcessName,Path,Company,Description" """,shell=True,stdout=subprocess.PIPE).stdout.decode('utf-8')
-        print(listening_processes)
-        Files.mk_file("netstat-listening_processes.txt",listening_processes)
-        
-        return(listening_processes)        
-        
-    def dns_checks():  
-        dnsChecks = subprocess.run("""powershell.exe "Get-DnsClientCache -Status 'Success' | Select Name, Data" """,shell=True,stdout=subprocess.PIPE).stdout.decode('utf-8')
-        print(dnsChecks)
-        Files.mk_file("dnsChecks.txt",dnsChecks)
-        return(dnsChecks)         
+def modules():
+    mods = {'gather':'Gather Information',
+            'query':'Query Windows events',
+            'network':'Show current network information'}
+    for k,v in mods.items():
+        print("{}\t=>\t\t{}".format(k,v))
+
+
+
+def write_csv():
+    nlist = []
+    ninput = input("Add your note:\n")
+    #ninput = ("{}\n".format(ninput))
+    nlist.append(ninput)
+    with open('notes.csv', 'a',newline='') as csvFile:
+        for x in nlist:
+            writer = csv.writer(csvFile)
+            writer.writerow([x])
+            csvFile.close()
+
+
+def show_notes():
+    with open('notes.csv', 'r') as csvFile:
+        reader = csv.reader(csvFile)
+        for row in reader:
+            print(row)
             
+            
+class Files():
+    def name_file(name):
+        timestr = time.strftime("%m-%d-%Y-")
+        filename = socket.gethostname()
+        inv_name = "Bluesploit_{time}{filename}_{module_name}".format(time=timestr,filename=filename,module_name=name)
+        return(inv_name)
     
+    def mk_file(name,*args):
+        filename = Files.name_file(name)
+        f = open("Investigations/{}/{}".format(Files.name_file(""),filename), "w",newline='')   
+        f.write(*args)
+        f.close()
+        
+
+os.makedirs("Investigations/{}".format(Files.name_file("")),exist_ok=True)
